c8e412024085b0c210894d99f75b3cd4ca76c8ab
MiuraKeiya <miurakeiya@yahoo.co.jp>
2023-07-07 00:41:37 +0900

    tailwind導入

---
+++ resources/ts/components/HomeComponents/FixtureResult.tsx
@@ -0,0 +1,5 @@
+export const FixtureResult = () => {
+  return (
+    <div className="text-red-500">試合日程・結果</div>
+  );
+};
---
+++ resources/ts/context/FixtureResultContext.tsx
@@ -0,0 +1,5 @@
+export const FixtureResultContext = () => {
+  return (
+    <div>dasdasd</div>
+  );
+};
