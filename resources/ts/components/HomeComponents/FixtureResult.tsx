dcd3c68df260660d68659e7bd9252ba90b400ad1
MiuraKeiya <miurakeiya@yahoo.co.jp>
2023-07-10 12:05:02 +0900

    今シーズン試合結果登録

--- resources/ts/components/HomeComponents/FixtureResult.tsx
+++ resources/ts/components/HomeComponents/FixtureResult.tsx
@@ -6,6 +6,23 @@ export const FixtureResult = () => {
     // 今シーズンの試合日程・試合結果・日付・エラーを取得
     const { fixtureResult, error } = useContext(FixtureResultContext);
 
+    // dateを変換
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        const options = {
+            month: "numeric",
+            day: "numeric",
+            weekday: "short",
+            hour: "2-digit",
+            minute: "2-digit",
+        };
+        const formattedDate = new Intl.DateTimeFormat("ja-JP", options).format(
+            date
+        );
+        const [datePart, timePart] = formattedDate.split(" ");
+        return <>{timePart}</>;
+    };
+
     return (
         <main className="flex-grow">
             <div>
@@ -17,25 +34,47 @@ export const FixtureResult = () => {
                                 return null; // エラーハンドリング
                             }
                             return (
-                                <table key={leagueId}>
-                                    <thead>
-                                        <tr>
-                                            <th>
-                                                {results[0].league.country}:{" "}
-                                                {results[0].league.name}
-                                            </th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {results.map((fixture, index) => (
-                                            <tr key={index}>
-                                                <td>
-                                                    {fixture.teams.away.name}
-                                                </td>
-                                            </tr>
-                                        ))}
-                                    </tbody>
-                                </table>
+                                <div key={leagueId}>
+                                    {results[0].league.country}:{" "}
+                                    {results[0].league.name}
+                                    <table>
+                                        <tbody>
+                                            {results.map((fixture, index) => (
+                                                <tr key={index}>
+                                                    <td>
+                                                        {formatDate(
+                                                            fixture.fixture.date
+                                                        )}
+                                                    </td>
+                                                    <td>
+                                                        {
+                                                            fixture.teams.home
+                                                                .name
+                                                        }
+                                                        <br />
+                                                        {
+                                                            fixture.teams.away
+                                                                .name
+                                                        }
+                                                    </td>
+                                                    <td >
+                                                        {fixture.score.fulltime
+                                                            .home !== null
+                                                            ? fixture.score
+                                                                  .fulltime.home
+                                                            : "-"}
+                                                        <br />
+                                                        {fixture.score.fulltime
+                                                            .away !== null
+                                                            ? fixture.score
+                                                                  .fulltime.away
+                                                            : "-"}
+                                                    </td>
+                                                </tr>
+                                            ))}
+                                        </tbody>
+                                    </table>
+                                </div>
                             );
                         }
                     )}
