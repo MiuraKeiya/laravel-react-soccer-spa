46817990d8c43a79e54eac3626f1e2365660e1d7
MiuraKeiya <miurakeiya@yahoo.co.jp>
2023-08-18 18:34:49 +0900

    sanctum設定

---
+++ resources/ts/pages/Auth/Login.tsx
@@ -0,0 +1,5 @@
+import { LoginForm } from "../../components/templates/Auth/LoginForm";
+
+export const Login = () => {
+    return <LoginForm />;
+};
---
+++ resources/ts/pages/Home.tsx
@@ -0,0 +1,7 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { HomeTemplate } from "../components/templates/Home/HomeTemplate";
+
+export const Home = () => {
+    return <HomeTemplate>dsadasd</HomeTemplate>;
+};
