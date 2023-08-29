0a8904012c6a84ff0762f849a26eea4c129f5b1f
MiuraKeiya <miurakeiya@yahoo.co.jp>
2023-08-29 13:03:12 +0900

    全てのリーグをお気に入り追加のモーダルに表示する

---
+++ resources/ts/components/organisms/Modal/ModalLeague.tsx
@@ -0,0 +1,35 @@
+import { useLeagueAPI } from "../../../hooks/useLeagueApi";
+import SportsSoccerIcon from "@mui/icons-material/SportsSoccer";
+import Tooltip from "@mui/material/Tooltip";
+import IconButton from "@mui/material/IconButton";
+
+export const ModalLeague = () => {
+    const { leagues, isLoading, error } = useLeagueAPI();
+
+    return (
+        <div className="text-white">
+            {leagues.map((league) => (
+                <div key={league.id}>
+                    <div className="flex items-center justify-between">
+                        <div className="flex items-center space-x-2 my-2">
+                            <img
+                                src={`https://media-3.api-sports.io/football/leagues/${league.id}.png`}
+                                alt="league"
+                                className="h-10 w-10 border border-white rounded-lg"
+                            />
+                            <p className="uppercase">{league.name}</p>
+                        </div>
+                        <IconButton sx={{ color: "#B0EE1B" }}>
+                            <Tooltip
+                                title="お気に入り追加する！"
+                                placement="left-start"
+                            >
+                                <SportsSoccerIcon />
+                            </Tooltip>
+                        </IconButton>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
