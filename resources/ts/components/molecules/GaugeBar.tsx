4f43deb14d57c90a4b3559617a815d2444332de5
MiuraKeiya <miurakeiya@yahoo.co.jp>
2023-09-13 15:01:17 +0900

    スタッツをゲージバーで表示

---
+++ resources/ts/components/molecules/GaugeBar.tsx
@@ -0,0 +1,65 @@
+import { useInView } from "react-intersection-observer";
+
+export const GaugeBar = ({ homeValue, awayValue, type }) => {
+    const homeNumericValue = parseFloat(homeValue) || 0;
+    const awayNumericValue = parseFloat(awayValue) || 0;
+
+    const totalValue = homeNumericValue + awayNumericValue;
+    const homePercentage = (homeNumericValue / totalValue) * 100;
+    const awayPercentage = (awayNumericValue / totalValue) * 100;
+
+    const showHomeGauge = homeNumericValue !== 0;
+    const showAwayGauge = awayNumericValue !== 0;
+
+    const [homeRef, homeInView] = useInView({
+        triggerOnce: true,
+        threshold: 0.2,
+    });
+
+    const [awayRef, awayInView] = useInView({
+        triggerOnce: true,
+        threshold: 0.2,
+    });
+
+    return (
+        <div className="mt-5 mx-3">
+            <div className="flex justify-between items-center my-2">
+                <span className="text-white text-[14px] font-bold">
+                    {homeValue || 0}
+                </span>
+                <span className="text-[14px] font-bold text-white">{type}</span>
+                <span className="text-right text-white text-[14px] font-bold">
+                    {awayValue || 0}
+                </span>
+            </div>
+            <div className="flex justify-center items-center mb-10">
+                <div
+                    ref={homeRef}
+                    className="rotate-180 bg-[#C8CDCD] h-[10px] w-full rounded-r-lg overflow-hidden"
+                >
+                    {showHomeGauge && (
+                        <div
+                            className={`rounded-r-lg bg-[#2e42f7] h-full ${
+                                homeInView ? "animate-flow-left" : ""
+                            }`}
+                            style={{ width: `${homePercentage}%` }}
+                        ></div>
+                    )}
+                </div>
+                <div
+                    ref={awayRef}
+                    className="bg-[#C8CDCD] h-[10px] w-full rounded-r-lg overflow-hidden"
+                >
+                    {showAwayGauge && (
+                        <div
+                            className={`rounded-r-lg bg-[#2ffb2c] h-full ${
+                                awayInView ? "animate-flow-left" : ""
+                            }`}
+                            style={{ width: `${awayPercentage}%` }}
+                        ></div>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
--- resources/ts/components/organisms/Games/TeamsStats.tsx
+++ resources/ts/components/organisms/Games/TeamsStats.tsx
@@ -1,7 +1,58 @@
+import { GaugeBar } from "../../molecules/GaugeBar";
+
 export const TeamsStats = ({ games }) => {
+    const translations = {
+        "Shots on Goal": "枠内シュート",
+        "Shots off Goal": "枠外シュート",
+        "Total Shots": "シュート数",
+        "Blocked Shots": "ブロックされたシュート数",
+        "Shots insidebox": "ボックス内のシュート数",
+        "Shots outsidebox": "ボックス外のシュート数",
+        Fouls: "ファウル",
+        "Corner Kicks": "コーナーキック",
+        Offsides: "オフサイド数",
+        "Ball Possession": "ボールポゼッション",
+        "Yellow Cards": "イエローカード",
+        "Red Cards": "レッドカード",
+        "Goalkeeper Saves": "ゴールキーパーセーブ数",
+        "Total passes": "パス本数",
+        "Passes accurate": "パス成功",
+        "Passes %": "パス精度",
+        expected_goals: "ゴール期待値",
+    };
+
+    const homeStatistics = games[0]?.json_detail.statistics[0].statistics;
+    const awayStatistics = games[0]?.json_detail.statistics[1].statistics;
+
+    // ホームとアウェイの統計データをフラット化し、typeをtranslationsに変換
+    const flattenedHomeStatistics = homeStatistics
+        ? homeStatistics.flat().map((stat) => ({
+              ...stat,
+              type: translations[stat.type] || stat.type,
+          }))
+        : [];
+
+    const flattenedAwayStatistics = awayStatistics
+        ? awayStatistics.flat().map((stat) => ({
+              ...stat,
+              type: translations[stat.type] || stat.type,
+          }))
+        : [];
+
     return (
         <div>
-            
+            <div className="bg-[#111931] text-[#EEEEEE] text-[18px] font-bold py-1 text-center rounded-b">
+                チームスタッツ
+            </div>
+            {flattenedHomeStatistics.map((homeStat, index) => (
+                <div key={index}>
+                    <GaugeBar
+                        homeValue={homeStat.value}
+                        awayValue={flattenedAwayStatistics[index].value}
+                        type={homeStat.type}
+                    />
+                </div>
+            ))}
         </div>
     );
 };
