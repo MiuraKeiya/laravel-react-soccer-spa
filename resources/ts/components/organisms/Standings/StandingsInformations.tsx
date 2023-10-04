15a6422c4caa35b7aa2f797591f372f5f70b246c
MiuraKeiya <miurakeiya@yahoo.co.jp>
2023-10-04 16:28:35 +0900

    順位一覧取得のAPIレスポンスにリーグ情報を追加

--- resources/ts/components/organisms/Standings/StandingsInformations.tsx
+++ resources/ts/components/organisms/Standings/StandingsInformations.tsx
@@ -1,7 +1,17 @@
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
+
 export const StandingsInformations = () => {
     return (
         <div className="bg-gradient-to-r from-[#1d2233] rounded h-[5rem] flex items-center">
-            <h1 className="text-white font-bold text-[35px] ml-6">順位一覧</h1>
+            <div className="flex items-center ml-6 space-x-3">
+                <EmojiEventsIcon
+                    className="text-[#EEEEEE]"
+                    sx={{ fontSize: "35px" }}
+                />
+                <h1 className="text-white font-bold text-[35px]">
+                    順位一覧
+                </h1>
+            </div>
         </div>
     );
 };
