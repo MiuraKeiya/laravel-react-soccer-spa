d07bd78fcb1863fe372c009eb62cb9455621524e
MiuraKeiya <miurakeiya@yahoo.co.jp>
2023-11-01 14:23:39 +0900

    各ページにフッター追加

--- resources/ts/pages/StandingsPage.tsx
+++ resources/ts/pages/StandingsPage.tsx
@@ -1,7 +1,7 @@
 import { StandingsTemplate } from "../components/templates/Standings/StandingsTemplate";
 import { Standings } from "../components/organisms/Standings/Standings";
 import { Header } from "../components/organisms/Header";
-import { Footer } from "../components/atoms/footer/Footer";
+import { Footer } from "../components/organisms/Footer/Footer";
 import { SideBar } from "../components/organisms/SideBar/SideBar";
 import { ScrollRestoration } from "react-router-dom";
 
--- resources/ts/pages/TeamPage.tsx
+++ resources/ts/pages/TeamPage.tsx
@@ -1,7 +1,7 @@
 import { TeamTemplate } from "../components/templates/Team/TeamTemplate";
 import { Team } from "../components/organisms/Team/Team";
 import { Header } from "../components/organisms/Header";
-import { Footer } from "../components/atoms/footer/Footer";
+import { Footer } from "../components/organisms/Footer/Footer";
 import { SideBar } from "../components/organisms/SideBar/SideBar";
 import { ScrollRestoration } from "react-router-dom";
 
