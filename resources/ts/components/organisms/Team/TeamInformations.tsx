17fe612176694ccbb7fd581b46654a1d48bae26f
MiuraKeiya <miurakeiya@yahoo.co.jp>
2023-12-01 22:20:36 +0900

    スタッツ表示

--- resources/ts/components/organisms/Team/TeamInformations.tsx
+++ resources/ts/components/organisms/Team/TeamInformations.tsx
@@ -13,16 +13,56 @@ export const TeamInformations = ({ informations, loading }) => {
                         className="flex justify-center mx-2 sm:mx-0"
                     >
                         <div className="flex items-center lg:justify-center space-x-6 my-5">
-                            <div className="border-2 border-[#3e4145] rounded-xl bg-black">
-                                <img
-                                    src={imageUrl(
-                                        "teams",
-                                        item.json_information.team.id,
-                                        "png"
-                                    )}
-                                    alt="team"
-                                    className="sm:h-24 sm:w-24 h-[4rem] w-[4rem]"
-                                />
+                            <div>
+                                <div className="border-2 border-[#3e4145] rounded-xl bg-black">
+                                    <img
+                                        src={imageUrl(
+                                            "teams",
+                                            item.json_information.team.id,
+                                            "png"
+                                        )}
+                                        alt="team"
+                                        className="sm:h-24 sm:w-24 h-[4rem] w-[4rem]"
+                                    />
+                                </div>
+                                <div>
+                                    <span className="text-[#EEEEEE] text-[11px]">
+                                        直近の試合
+                                    </span>
+                                    <div className="flex pt-1">
+                                        {item.json_statistics.form ? (
+                                            item.json_statistics.form
+                                                .slice(-5)
+                                                .split("")
+                                                .map((char, index) => (
+                                                    <div
+                                                        key={index}
+                                                        className="flex items-center justify-center sm:h-4 sm:w-4 h-3 w-3 sm:mx-[1px]"
+                                                    >
+                                                        <span
+                                                            className={`text-black sm:text-[14px] text-[10px] font-semibold h-full w-full flex items-center justify-center ${
+                                                                char === "W"
+                                                                    ? "bg-[#44C486]"
+                                                                    : char ===
+                                                                      "D"
+                                                                    ? "bg-[#9198AC]"
+                                                                    : char ===
+                                                                      "L"
+                                                                    ? "bg-[#E66F5D]"
+                                                                    : ""
+                                                            }`}
+                                                        >
+                                                            {char}
+                                                        </span>
+                                                    </div>
+                                                ))
+                                        ) : (
+                                            <div className="text-white text-opacity-40 text-[14px]">
+                                                該当なし
+                                            </div>
+                                        )}
+                                    </div>
+                                </div>
                             </div>
                             <div className="text-[#EEEEEE] flex items-center space-x-11">
                                 <div>
